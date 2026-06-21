a002bdb06866d82e31e0076911b503f5952d8025
sctale <sctale@163.com>
2026-06-21 13:28:22 +0800

    chore: 清理其他项目脚本

--- encode-image.ps1
+++
@@ -1,3 +0,0 @@
-$ErrorActionPreference = "Continue"
-$base64 = [Convert]::ToBase64String([IO.File]::ReadAllBytes("d:\V-Coding\anti-car-sickness-rn\Screenshot_20260531_134259_anti-car-sickness-rn.jpg"))
-Write-Host "Image encoded, length: $($base64.Length)"
